817bb4f9f9504189ccd982ce855da41333b3c17c
rolansor <rolansor@hotmail.com>
2015-11-25 22:36:43 -0500

    Testeando el repositorio.

--- testfiles/server.c
+++ testfiles/server.c
@@ -1,5 +1,3 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,48 +6,72 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+//Si algo sale mal, salimos.
 void error(const char *msg)
 {
     perror(msg);
     exit(1);
 }
 
+//Main.
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno;
-     socklen_t clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, 
-                 (struct sockaddr *) &cli_addr, 
-                 &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     close(newsockfd);
-     close(sockfd);
-     return 0; 
+	//Variables
+	int sockfd, newsockfd, portno;
+	socklen_t clilen;
+	char buffer[256];
+	struct sockaddr_in serv_addr, cli_addr;
+	int n;
+
+	// Si no pasamos el puerto como parametro, mostramos mensaje y salimos.
+	if (argc < 2) {
+	 fprintf(stderr,"ERROR, no port provided\n");
+	 exit(1);
+	}
+
+	//Creamos el socket.
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	//Si no se puede crear el socket mostramos error.
+	if (sockfd < 0) 
+	error("ERROR opening socket");
+	
+	//Igual que memset(), bzero deprecated.
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	
+	//Obtenemos el puerto recibimos el parametro de entrada y lo convertimos a int.
+	portno = atoi(argv[1]);
+
+	//Configuramos el servidor.
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+
+	//Hacemos binding con la estructura del sockadrr a la estructura serv_addr.
+	if (bind(sockfd, (struct sockaddr *) &serv_addr,
+	      sizeof(serv_addr)) < 0) 
+	      error("ERROR on binding");
+
+	listen(sockfd,5);
+	//Ola k ase
+	clilen = sizeof(cli_addr);
+	newsockfd = accept(sockfd, 
+		 (struct sockaddr *) &cli_addr, 
+		 &clilen);
+
+	if (newsockfd < 0) 
+	  error("ERROR on accept");
+
+	bzero(buffer,256);
+	n = read(newsockfd,buffer,255);
+
+	if (n < 0) error("ERROR reading from socket");
+	printf("Here is the message: %s\n",buffer);
+	n = write(newsockfd,"I got your message",18);
+	if (n < 0) error("ERROR writing to socket");
+
+	close(newsockfd);
+	close(sockfd);
+
+	return 0; 
 }
